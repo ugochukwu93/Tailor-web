d777c390b8c83242795d6b287526d4bf0e0a9225
Ugochukwu Nwadike <nwadikeugochukwu93@gmail.com>
2022-03-05 18:25:19 +0100

    deploying to server

---
+++ server.js
@@ -0,0 +1,18 @@
+
+const path = require('path');
+const express = require('express');
+const serverStatic = require('server-static')
+
+const app = express();
+
+// const publicPath = path.join(__dirname, '..', 'public');
+app.use('/', serverStatic(path.join(__dirname, '/dist')));
+
+app.get(/.*/, (req, res) => {
+    res.sendFile(path.join(__dirname, '/dist/index.html'));
+ });
+ const port = process.env.PORT || 3000;
+ app.listen(port)
+    console.log(`app is running on port: ${port}`);
+
+
